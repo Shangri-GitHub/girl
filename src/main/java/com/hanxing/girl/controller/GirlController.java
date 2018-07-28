71e4611c350a58d0c744671c5ed95d2ed0d30cbb
韩星 <467527309@qq.com>
2018-07-28 15:02:05 +0800

    统一异常处理，对返回值做处理

--- src/main/java/com/hanxing/girl/controller/GirlController.java
+++ src/main/java/com/hanxing/girl/controller/GirlController.java
@@ -2,11 +2,12 @@ package com.hanxing.girl.controller;
 
 
 import com.hanxing.girl.domain.Girl;
+import com.hanxing.girl.domain.Result;
 import com.hanxing.girl.repository.GirlRepository;
 import com.hanxing.girl.service.GirlService;
+import com.hanxing.girl.utils.ResultUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -36,14 +37,14 @@ public class GirlController {
 
     // 添加一个女生
     @PostMapping(value = "/girls")
-    public Girl addGirl(@Valid Girl girl, BindingResult bindingResult) {
+    public Result addGirl(@Valid Girl girl, BindingResult bindingResult) {
+
         if(bindingResult.hasErrors()){
-            System.out.println(bindingResult.getFieldError().getDefaultMessage());
-            return null;
+            return ResultUtils.error(bindingResult.getFieldError().getDefaultMessage());
         }
         girl.setAge(girl.getAge());
         girl.setCupSize(girl.getCupSize());
-        return girlRepository.save(girl);
+        return ResultUtils.success(girlRepository.save(girl));
     }
 
     // 通过id查询一个女生
--- src/main/java/com/hanxing/girl/repository/GirlRepository.java
+++ src/main/java/com/hanxing/girl/repository/GirlRepository.java
@@ -2,10 +2,11 @@ package com.hanxing.girl.repository;
 
 import com.hanxing.girl.domain.Girl;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-
+@Component
 public interface GirlRepository extends JpaRepository <Girl, Integer> {
     //通过年龄来查询
     public List <Girl> findByAge(Integer age);
