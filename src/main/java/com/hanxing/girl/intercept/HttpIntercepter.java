4debf68396bf01223d47c8d780131ff8d3fa7c6f
韩星 <467527309@qq.com>
2018-07-28 13:52:53 +0800

    添加logger方法记日志

--- src/main/java/com/hanxing/girl/intercept/HttpIntercepter.java
+++ src/main/java/com/hanxing/girl/intercept/HttpIntercepter.java
@@ -1,20 +1,32 @@
 package com.hanxing.girl.intercept;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class HttpIntercepter {
+
+    private final static Logger logger = LoggerFactory.getLogger(HttpIntercepter.class);
+
     @Pointcut("execution(public * com.hanxing.girl.controller.GirlController.*(..))")
     public void log() {
 
     }
+
     @Before("log()")
-    public void doBefore(){
-        
+    public void doBefore() {
+        logger.info("11");
+    }
+
+    @After("log()")
+    public void doAftere() {
+        logger.info("222");
     }
 
 
